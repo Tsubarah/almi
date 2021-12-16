5ea374fa7580827d9fc9b0420ec4c79779637236
Benjamin <Tsubarah@hotmail.se>
2021-12-16 21:22:08 +0100

    added login

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const heroBtn = document.querySelector('.aboutLoan');
+const heroBtn = document.querySelectorAll('.aboutLoan');
 const dropdownContent = document.querySelector('.dropdownContent');
+const readMoreBtn = document.querySelector('.readMore');
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
+heroBtn.forEach(link => {
+    link.addEventListener('click', () => {
+        dropdownContent.classList.toggle('hide');
+    });
+})
 
-heroBtn.addEventListener('click', () => {
-    dropdownContent.classList.toggle('hide');
-});
+readMoreBtn.addEventListener('click', () => {
+    window.open();
+})
 
