1209893812f6691a5191fc3ddd48d9faaad082c7
Benjamin Tsubarah <benjamin@tsubarah.com>
2021-12-16 14:28:07 +0100

    added icons and continued with layout

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const heroBtn = document.querySelector('.aboutLoan');
 const dropdownContent = document.querySelector('.dropdownContent');
-const applyBtn = document.querySelectorAll('.applyBtn');
 var myModal = document.getElementById('myModal')
 var myInput = document.getElementById('myInput')
 
@@ -9,9 +8,3 @@ heroBtn.addEventListener('click', () => {
     dropdownContent.classList.toggle('hide');
 });
 
-
-
-
-applyBtn.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
